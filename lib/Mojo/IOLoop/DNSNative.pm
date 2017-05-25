69cfe348296c6482cf06b4dd0632f5118d73a094
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2017-05-25 14:14:19 +0200

    Apply perltidy

--- lib/Mojo/IOLoop/DNSNative.pm
+++ lib/Mojo/IOLoop/DNSNative.pm
@@ -1,4 +1,5 @@
 package Mojo::IOLoop::DNSNative;
+
 # ABSTRACT: Async native DNS lookup
 use Mojo::Base qw/ Mojo::Base /;
 
@@ -7,26 +8,40 @@ use Mojo::IOLoop;
 use Net::DNS::Native;
 use Socket qw/ getnameinfo NI_NUMERICHOST NIx_NOSERV /;
 
-has NDN => sub { state $NDN = Net::DNS::Native->new(pool => 5, extra_thread => 1) };
-has reactor => sub { Mojo::IOLoop->singleton->reactor });
+has NDN =>
+  sub { state $NDN = Net::DNS::Native->new(pool => 5, extra_thread => 1) };
+has reactor => sub { Mojo::IOLoop->singleton->reactor };
 has timeout => 10;
 
 sub lookup {
-    my ($self, $address, $cb) = @_;
+  my ($self, $address, $cb) = @_;
 
-    my $reactor = $self->reactor;
-    my $ndn = $self->NDN;
+  my $reactor = $self->reactor;
+  my $ndn     = $self->NDN;
 
-    my $handle = $ndn->getaddrinfo($address, undef);
-    $reactor->timer($self->timeout, sub { $ndn->timedout($handle); $reactor->remove($handle); $cb->('DNS lookup timed out'); });
-    $reactor->io($handle => sub {
-            my $reactor = shift;
-            $reactor->remove($handle);
-            my ($err, @res) = $ndn->get_result($handle);
+  my $handle = $ndn->getaddrinfo($address, undef);
+  my $tid = $reactor->timer(
+    $self->timeout,
+    sub {
+      $ndn->timedout($handle);
+      $reactor->remove($handle);
+      $cb->('DNS lookup timed out');
+    }
+  );
+  $reactor->io(
+    $handle => sub {
+      my $reactor = shift;
+      $reactor->remove($handle);
+      $reactor->remove($tid);
+      my ($err, @res) = $ndn->get_result($handle);
 
-            $cb->($err, uniq map { (getnameinfo($_->{addr}, NI_NUMERICHOST, NIx_NOSERV))[1] } @res);
-        }
-    )->watch($handle, 1, 0);
+      $cb->(
+        $err,
+        uniq map { (getnameinfo($_->{addr}, NI_NUMERICHOST, NIx_NOSERV))[1] }
+          @res
+      );
+    }
+  )->watch($handle, 1, 0);
 }
 
 1;
